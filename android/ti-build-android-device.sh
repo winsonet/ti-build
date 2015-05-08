b0244f274ca0c9553c4287cda838687c2b02f1d8
Inwdragon <kanokpoljuntum@hotmail.com>
2015-05-08 11:16:02 +0700

    update help

--- android/ti-build-android-device.sh
+++ android/ti-build-android-device.sh
@@ -119,13 +119,11 @@ show_help ()
 {
     me="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
     echo "usage: ${me} [options]"
-    # echo "       -gs=<value>, --get-model=<value>\t\tdisplay list sdk version"
-    # echo "       -s=<value>, --sdk=<value>\t\tthe sdk version [$(echo $(get_sdks) | sed 's/|#|/, /g')]"
-    # echo "       -gdn=<value>, --get-device-name=<value>\tdisplay list device name"
-    # echo "       -dn=<value>, --device-name=<value>\tthe device name"
-    # echo "       -gdi=<value>, --get-device-id=<value>\tdisplay list device id"
-    # echo "       -di=<value>, --device-id=<value>\t\tthe device id"
-    # echo "       -d=<value>, --dir=<value>\t\tthe directory titanium project"
+    echo "       -di=<value>, --device-id=<value>\t\t\t\tdisplay list device name"
+    echo "       -gm=<value>, --get-model=<value>\t\t\t\tthe device model"
+    echo "       -gdiwm=<value>, --get-device-id-with-model=<value>\tdisplay device"
+    echo "       -gmwdi=<value>, ---get-model-with-device-id=<value>\tdisplay model"
+    echo "       -d=<value>, --dir=<value>\t\t\t\tthe directory titanium project"
 }
 
 hash ${SHELL_TI} > /dev/null 2>&1 || {
